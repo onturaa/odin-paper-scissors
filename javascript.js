e7528181e1510a20793cc2c7c00f62ece7752778
onturaa <174931989+onturaa@users.noreply.github.com>
2025-01-06 20:15:23 -0500

    Create Doc and Write getComputerChoice Logic
    Create basic html layout and attach external javascript
    doc. Write logic for getComputerChoice() so one of the
    following string values: "rock", "paper", or "scissors"
    is returned.

---
+++ javascript.js
@@ -0,0 +1,24 @@
+const OPTIONS = 3;
+
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * OPTIONS);
+    if (number === 0) {
+        return "rock";
+    }
+    else if (number === 1) {
+        return "paper";
+    }
+    else if (number === 2) {
+        return "scissors";
+    }
+    else {
+        return "error";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
